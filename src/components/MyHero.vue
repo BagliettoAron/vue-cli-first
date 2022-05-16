bedd47b6620c0c7d561d799b5e6709b8bbcf1d4f
@BagliettoAron <aronbaglietto88@gmail.com>
2022-05-16 17:34:24 +0200

    Add: milestone & bonus

---
+++ src/components/MyHero.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <img
+      src="../assets/_imaging-of-beautiful-landscape-with-nice_16823510_high.webp"
+      alt=""
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyBanner",
+};
+</script>
+
+<style></style>
